cbb15fd269169ebaa0707fb24bc0b82c7359cb23
Jayden Liaw <liawjianwei@outlook.com>
2024-09-06 17:42:38 +0800

    Deploy backend to Heroku

---
+++ README.md
@@ -0,0 +1,42 @@
+# debuggingDragons
+
+## Set up and how to run the app
+
+1. Make sure python and vscode are installed.
+2. Add Conda to the System PATH
+      For Windows 10/11:
+      1. Open Environment Variables:
+         Right-click on the Start menu, select System.
+         Click Advanced system settings > Environment Variables.
+      2. Edit the PATH Variable:
+      Under System variables (or User variables), find and select Path.
+      Click Edit.
+      3. Add the New Paths:
+      Click New and add:
+         C:\Users\${user}\anaconda3
+         C:\Users\${user}\anaconda3\Scripts
+         C:\Users\${user}\anaconda3\Library\bin
+      --------------------------------------------------------   
+      For Linux/macOS:
+      1. Open Terminal.
+      2. Edit the Shell Profile:
+      Use a text editor to open your shell profile file:
+         Bash: nano ~/.bashrc
+         Zsh: nano ~/.zshrc
+      3. Add Conda to PATH:
+      Add the following line:
+         export PATH="$HOME/anaconda3/bin:$PATH"
+      4. Apply Changes:
+      Save and close the file.
+      Run source ~/.bashrc or source ~/.zshrc.
+
+3. Install the dependencies in `environment.yml` via one of these methods:
+   - Clone the conda environment (probably better). Download conda/conda navigator, in conda navigator import `environment.yml`, launch vscode under 'Flask' environment. Or just create the conda environment in CLI using `conda env create -f environment.yml`, `conda activate Flask`
+   - Just pip install the missing dependencies. Important ones are flask and flask alchemy and anything you are missing when you run the app. `pip install flask flask-sqlalchemy`.
+4. Launch vscode -> open folder -> this repo
+5. In your terminal in VSC, navigate to this directory containing app.py. Run `python app.py`.
+6. Open your browser, go to `http://localhost:5000/`.
+
+## Google docs
+
+https://docs.google.com/document/d/1m8FPXPHyh6ocvPlVjcdsX1Ma0YKQ7qh7JQH9lEzI2T8/edit
---
+++ app.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_cors import CORS  # Import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+# Rest of your app code...
+
+
+class pr(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pr_id = db.Column(db.String(200), nullable=False)
+    sourceBranchName = db.Column(db.String(200), nullable=False)
+    targetBranchName = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(1000), nullable=False) # Cannot be blank, max 1000 chars
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) # Automatically set the time when it is added
+    def __repr__(self):
+        return '<PR %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    """
+    Main page. Endpoint for the root of the Flask app.
+    Listens for incoming payload from the bitbucket webhook, checks if it is a pull request.
+    If so, processes the PR data and sends to chatgpt for review. 
+    Also adds the PR data to the db and redirects the user to a summary page of the code review.
+    """
+    if request.method == 'POST':
+        data = request.json
+        if 'pullrequest' in data:
+            pr_data = data['pullrequest']
+            pr_id = pr_data['id']
+            source_branch = pr_data['source']['branch']['name']
+            target_branch = pr_data['destination']['branch']['name']
+            files_diff = get_files_diff(pr_id)
+            new_pr_diff = pr(pr_id=pr_id, sourceBranchName=source_branch, targetBranchName=target_branch, content=files_diff)
+            process_files_diff(files_diff)
+            try:
+                db.session.add(new_pr_diff)
+                db.session.commit()
+                return redirect('/summary/')
+            except:
+                return 'There was an issue adding a new pull request to the db.'
+            
+        return "OK", 200
+    else:
+        return render_template('index.html')
+
+# Bitbucket credentials and repository details
+BITBUCKET_USERNAME = 'your_username'
+BITBUCKET_REPO_SLUG = 'your_repo_slug'
+ACCESS_TOKEN = 'your_access_token' # Need to find out how to get our access token
+
+def get_files_diff(pr_id):
+    """
+    Gets the file diff from bitbucket based on the given pr_id, and extracts the required data into 'detailed_changes'.
+    'detailed_changes' is a list of files that have changed. For each file, there is a dict containing the path of the file,
+    a list of lines added, and a list of lines removed.
+
+    Example of files_diff:
+    files_diff = [
+    {'path': "src/main.py",
+        'lines_added': ["print('Hello, world!')", "print('Bye, world!')"],
+        'lines_removed': ["print('Remove me')]"]
+        }, 
+
+    {'path': "src/quickMaths.py",
+        'lines_added': ["x = 1", "y = 2", "z = 3"],
+        'lines_removed': []
+        }
+    ]
+    """
+    url = f"https://api.bitbucket.org/2.0/repositories/{BITBUCKET_USERNAME}/{BITBUCKET_REPO_SLUG}/pullrequests/{pr_id}/diff"
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}'
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        diff_data = response.json()
+        
+        detailed_changes = []
+        for file in diff_data.get('values', []):
+            file_info = {
+                'path': file['path'],
+                'lines_added': [],
+                'lines_removed': []
+            }
+            
+            for line in file.get('lines', []):
+                if line['type'] == 'add':
+                    file_info['lines_added'].append(line['content'])
+                elif line['type'] == 'remove':
+                    file_info['lines_removed'].append(line['content'])
+            
+            detailed_changes.append(file_info)
+        
+        return detailed_changes
+    else:
+        print(f"Failed to retrieve PR diff: {response.status_code} - {response.text}")
+        return []
+    
+def process_files_diff(files_diff):
+    """
+    Processes the files diff and send it to chatgpt to review.
+    """
+    return
+
+@app.route('/summary/')
+def summary():
+    """
+    This page will contain the code review of the latest PR made.
+    """
+    return render_template('summary.html')
+
+@app.route('/chatbot/')
+def chatbot():
+    """
+    This page will allow the user to chat with the AI about its suggested changes to the latest PR.
+    """
+    return render_template('chatbot.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
